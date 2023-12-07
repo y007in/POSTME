05ab3389706cb0203d45259e5bfabd912321366b
yoojin <yoojin4719@gmail.com>
2023-12-07 15:33:46 +0900

    img

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,17 +6,28 @@ const Profile = () => {
   const openbox = new Array(postboxLength).fill(0);
   return (
     <div className="forme">
-      <img
-        className="profile"
-        src={`${process.env.PUBLIC_URL}Assets/forme.png`}
-        alt="profile"
-      />
-      <h2 className="name">장유진</h2>
-      <div className="balloon">
-        {openbox.length <= 10
-          ? "나에 대해 더 알아가봐요"
-          : "나의 모습을 잘 알고 있어요"}
-      </div>
+      {openbox.length <= 10 ? (
+        <>
+          <img
+            className="profile"
+            src={`${process.env.PUBLIC_URL}Assets/forme.png`}
+            alt="profile"
+          />
+          <h2 className="name">장유진</h2>
+          <div className="balloon">나에 대해 더 알아가봐요</div>
+        </>
+      ) : (
+        <>
+          {" "}
+          <img
+            className="profile2"
+            src={`${process.env.PUBLIC_URL}Assets/forme2.png`}
+            alt="profile"
+          />
+          <h2 className="name">장유진</h2>
+          <div className="balloon">나의 모습을 잘 알고 있어요</div>
+        </>
+      )}
     </div>
   );
 };
